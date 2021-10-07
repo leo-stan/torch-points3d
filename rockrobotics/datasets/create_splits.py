2067217afc324d5f78dce99a8635d70b82d50da6
Christopher Lee <csl170000@utdallas.edu>
2021-10-07 08:57:13 -0600

    updated splits

--- rockrobotics/datasets/create_splits.py
+++ rockrobotics/datasets/create_splits.py
@@ -4,9 +4,19 @@ import os.path as osp
 import random
 import copclib as copc
 from joblib import Parallel, delayed
+import numpy as np
+from torch.functional import _return_counts
+from tqdm import tqdm
+import math
+
+def m_to_ft(m):
+    return m * 3.281
 
 resolution = 0.5
-datasets = ["lux", "sasche", "tyrol"]
+target_tile_size = 100
+max_resolution = 5
+test_res = 20
+datasets = ["autzen"]#["lux", "sasche", "tyrol"]
 path = "/media/nvme/pcdata"
 splits = {'train': 0.8, 'val': 0.1, 'test': 0.1}
 version = "v1"
@@ -22,33 +32,77 @@ def get_split():
         return "train"
 
 def split_dataset(dataset):
-    dataset_splits = {"train": {}, "test": {}, "val": {}}
 
     dataset_dir = osp.join(path, dataset, "copc")
-    for datafile in os.listdir(dataset_dir):
+    
+    def split_file(datafile):        
         datafile_path = osp.join(dataset_dir, datafile, "octree.copc.laz")
         if not osp.exists(datafile_path):
-            continue
+            return None
 
-        try:
+        #try:     
+        if True:
             reader = copc.FileReader(datafile_path)
-            nodes = reader.GetNodesAtResolution(resolution)
+            header = reader.GetLasHeader()
+            depth = reader.GetDepthAtResolution(resolution)
+            max_depth = reader.GetDepthAtResolution(max_resolution)
+            max_depth = reader.GetDepthAtResolution(max_resolution)
+
+            span = header.GetSpan()
+            nearest_depth = math.floor(math.log2(span/target_tile_size))
+            num_voxels = 2**nearest_depth
+            tile_size = span / num_voxels
+
+            datafile_splits = {"train": {}, "test": {}, "val": {}}
+
+            bounds = header.GetBounds()
+
+            min_bounds = np.array([bounds.x_min, bounds.y_min, bounds.z_min])
+
+            min = np.array(min_bounds, copy=True)
             
-            for split in dataset_splits.keys():
-                # initialize file split
-                dataset_splits[split][datafile] = []
+            xy = {}
 
-            for node in nodes:
-                dataset_splits[get_split()][datafile].append(str(node.key))
+            for x in range(num_voxels):
+                min[0] = min_bounds[0] + tile_size * x
+                for y in range(num_voxels):
+                    xy[(nearest_depth, x,y)] = []
+                    min[1] = min_bounds[1] + tile_size * y
+                    for z in range(num_voxels):
+                        min[2] = min_bounds[2] + tile_size * z
+                        max = min + tile_size
 
+                        if min[0] > bounds.x_max or min[1] > bounds.y_max or min[2] > bounds.z_max or max[0] < bounds.x_min or max[1] < bounds.y_min or max[2] < bounds.z_min:
+                            continue
+
+                        key = copc.VoxelKey(nearest_depth, x, y, z)
+
+                        while key.d >= max_depth:
+                            if reader.FindNode(key).IsValid():
+                                xy[(nearest_depth, x,y)].append(z)
+                                #datafile_splits[get_split()].append([nearest_depth, x, y, z])
+                                break
+                            key = key.GetParent()
+            
+            for k,v in xy.items():
+                if len(v) > 0:
+                    datafile_splits[get_split()][str(k)] = v
+            
             reader.Close()
-        except (Exception) as err:
-            print(datafile_path + " " + err)
+            return (datafile, datafile_splits)
+    
+    x = Parallel(n_jobs=-1)(delayed(split_file)(datafile) for datafile in tqdm(os.listdir(dataset_dir)))
+    dataset_splits = {"train": {}, "test": {}, "val": {}}
+    x = [z for z in x if z is not None]
+    for f, d in x:
+        for split in dataset_splits.keys():
+            dataset_splits[split][f] = d[split]
 
     with open(osp.join(dataset_dir, "splits-%s.json" % version), 'w', encoding='utf-8') as f:
         json.dump(dataset_splits, f, ensure_ascii=False, indent=4)
 
-Parallel(n_jobs=-1)(delayed(split_dataset)(dataset) for dataset in datasets)
+split_dataset(datasets[0])
+#Parallel(n_jobs=-1)(delayed(split_dataset)(dataset) for dataset in datasets)
 
 with open(osp.join(path, "dataset-%s.json" % version), 'w', encoding='utf-8') as f:
     json.dump(
