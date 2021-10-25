34503021b54b760e1cb4cd0e43466f943ae8df9b
Chris Lee <csl170000@utdallas.edu>
2021-10-25 13:33:31 -0600

    update cpu build script

--- CONDA_BUILD_CPU.sh
+++ CONDA_BUILD_CPU.sh
@@ -7,20 +7,20 @@ conda activate tp3d-cpu
 # pytorch
 conda install pytorch=1.9.1 cpuonly -c pytorch -c conda-forge -y
 # pyg
-conda install pytorch-geometric<2.0.0 -c rusty1s -y
+conda install "pytorch-geometric<2.0.0" -c rusty1s -y
 pip install torch-points-kernels --no-cache-dir
 
 # mink
 export SPARSE_BACKEND=minkowski
 pip install torch ninja
-pip install -U MinkowskiEngine --install-option="--blas=openblas" -v --no-deps
+pip install -U MinkowskiEngine -v --no-deps
 
 # other requires
 pip install wandb~=0.8.18 hydra-core~=1.0.0 torchnet~=0.0.4 tqdm tensorboard plyfile gdown h5py joblib
 pip install pytorch-metric-learning==0.9.87.dev0 --no-deps -U
-
-
 conda install -c conda-forge pybind11 matplotlib -y
+pip install -e .
+
 git clone https://github.com/RockRobotic/laz-perf.git
 cd laz-perf
 git checkout 070f39bf0fcd7126ca4138f3791c693547d85a68
