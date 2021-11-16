90c10783f3d3613a1d3bab05435817d712352a8a
Leo Stanislas <leo.stanislas@gmail.com>
2021-11-15 21:44:01 -0400

    Update get_only_depth.py.

--- utils/local/debug/get_only_depth.py
+++ utils/local/debug/get_only_depth.py
@@ -3,19 +3,13 @@ import copclib as copc
 
 reader = copc.FileReader("/media/nvme/pcdata/autzen/copc/split_0/octree.copc.laz")
 
-# Copy the header to the new file
-cfg = copc.LasConfig(reader.GetLasHeader(), reader.GetExtraByteVlr())
-
 # Now, we can create our actual writer, with an optional `span` and `wkt`:
 writer = copc.FileWriter(
     "/media/nvme/pcdata/autzen/copc/split_0/autzen-resolution-trimmed.copc.laz",
-    cfg,
-    reader.GetCopcHeader().span,
-    reader.GetWkt(),
+    reader.copc_config,
 )
 
-root_page = writer.GetRootPage()
-for node in reader.GetAllChildren():
+for node in reader.GetAllNodes():
     if node.key.d == 4:
         points = reader.GetPoints(node)
         for point in points:
@@ -24,7 +18,7 @@ for node in reader.GetAllChildren():
             point.Blue = node.key.z
             point.Intensity = node.key.d
             point.PointSourceID = node.key.x + node.key.y
-        writer.AddNode(root_page, node.key, points)
+        writer.AddNode(node.key, points)
 
 # Make sure we call close to finish writing the file!
 writer.Close()
